d738fb653b62be4e124a2c6ed84fe42fa887b698
Sean Boonzaier <boonzaiersa@gmail.com>
2023-01-06 11:14:46 +0200

    Prettier the text

--- src/Components/Meals/MenuCard.js
+++ src/Components/Meals/MenuCard.js
@@ -1,15 +1,13 @@
 import MealCard from "./MealCard";
-import classes from './MenuCard.module.css'
+import classes from "./MenuCard.module.css";
 
 const MenuCard = (props) => {
-  const mealsList = props.mealItems.map((meal) => (
-    <MealCard mealItem={meal}  />
-  ));
+  const mealsList = props.mealItems.map((meal) => <MealCard mealItem={meal} />);
 
   return (
-  <div className={classes.menu}>
-    <ul >{mealsList}</ul>
-  </div>
+    <div className={classes.menu}>
+      <ul>{mealsList}</ul>
+    </div>
   );
 };
 
