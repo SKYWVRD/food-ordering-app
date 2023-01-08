816c009a1f29ca434f2ea8fc39ef6262d7a1d9e6
Sean Boonzaier <boonzaiersa@gmail.com>
2023-01-08 10:52:26 +0200

    Add functionality to allow closing of cart when clicking modal backdrop

--- src/Components/UI/Modal.js
+++ src/Components/UI/Modal.js
@@ -3,7 +3,7 @@ import ReactDom from 'react-dom'
 import classes from './Modal.module.css'
 
 const Backdrop = props => {
-    return <div className={classes.backdrop}></div>
+    return <div className={classes.backdrop} onClick={props.onShowCart}></div>
 }
 
 const ModalOverlay = props => {
@@ -16,7 +16,7 @@ const portalElement = document.getElementById('overlays');
 
 const Modal = props => {
     return <React.Fragment>
-        {ReactDom.createPortal(<Backdrop />, portalElement)}
+        {ReactDom.createPortal(<Backdrop onShowCart={props.onShowCart}/>, portalElement)}
         {ReactDom.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
     </React.Fragment>
 
