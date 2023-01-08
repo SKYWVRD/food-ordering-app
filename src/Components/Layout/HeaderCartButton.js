a6a96326381a7ecb0117fa5812771a442f109abe
Sean Boonzaier <boonzaiersa@gmail.com>
2023-01-08 11:41:43 +0200

    Add cart context to header cart button with number of items

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,14 +1,24 @@
+import {useContext} from 'react'
+
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context"
 
 const HeaderCartButton = (props) => {
+
+  const cartCtx = useContext(CartContext)
+
+  const numberOfCartitems = cartCtx.items.reduce((currentValue, item)=>{
+    return currentValue + item.amount
+  }, 0)
+
   return (
     <button className={classes.button} onClick={props.onShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartitems}</span>
     </button>
   );
 };
